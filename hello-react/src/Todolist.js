31347f4fefe6ada7b98dddbaf6f3a65d129d9a8e
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-09-16 15:50:56 +0200

    Correction exercice

--- hello-react/src/Todolist.js
+++ hello-react/src/Todolist.js
@@ -3,19 +3,53 @@ import React, { Component } from "react";
 class Todolist extends Component {
   state = {
     todos: ["Pain", "Lait", "Beurre"],
+    newTodo: "",
+  };
+
+  // handleDelete = (event) => {
+  //   console.log(event.target.innerText);
+  // }
+  handleDelete = (todo) => {
+    // créé un nouveau tableau
+    // changement immuable
+    this.setState({
+      todos: this.state.todos.filter((t) => t !== todo),
+    });
+  };
+  handleChange = (event) => {
+    this.setState({
+      newTodo: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // met à jour le tableau existant
+    // changement muable
+    // this.state.todos.push(this.state.newTodo);
+    // this.forceUpdate();
+
+    // créé un nouveau tableau
+    // changement immuable
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: "",
+    });
   };
   render() {
+    const items = this.state.todos.map((todo) => (
+      <div key={todo} className="TodoItem">
+        {todo}
+        <button onClick={() => this.handleDelete(todo)}>-</button>
+      </div>
+    ));
     return (
       <div className="TodoList">
-        <form>
-          <input />
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.newTodo} onChange={this.handleChange} />
           <button>+</button>
         </form>
-        <div className="TodoItems">
-          <div className="TodoItem">A</div>
-          <div className="TodoItem">B</div>
-          <div className="TodoItem">C</div>
-        </div>
+        <div className="TodoItems">{items}</div>
       </div>
     );
   }
